05c8d972220fc6cb0b9e6e9c990efa8588a56290
tecrodrigocastro <tec.rodrigocastro@gmail.com>
2024-04-20 09:30:49 -0300

    HEY-6: wip

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Question;
+use Illuminate\Http\{RedirectResponse, Request};
+
+class QuestionController extends Controller
+{
+    public function store(Request $request): RedirectResponse
+    {
+        $attributes = $request->validate([
+            'question' => ['required'],
+        ]);
+        Question::query()->create($attributes);
+
+        return to_route('dashboard');
+    }
+}
---
+++ tests/Feature/CreateAQuestionTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\User;
+
+use function Pest\Laravel\{actingAs, assertDatabaseCount, assertDatabaseHas, post};
+
+it(
+    'should be able to create a new question bigger than 255 characters',
+    function () {
+
+        /*      //Arrange :: PREPARAR
+             $user = User::factory()->create();
+             // login as user
+             actingAs($user);
+
+             //Act :: AGIR
+             $request = post('question.store', [
+                 'question' => str_repeat('*', 260) . '?',
+             ]);
+
+             //Assert :: VERIFICAR
+             $request->assertRedirect('dashboard');
+             assertDatabaseCount('questions', 1);
+             assertDatabaseHas('questions', [
+                 'question' => str_repeat('*', 260) . '?',
+             ]); */
+    }
+);
+
+it('should check if ends with questions mark ?', function () {
+
+});
+
+it('should have at least 10 characters', function () {
+
+});
